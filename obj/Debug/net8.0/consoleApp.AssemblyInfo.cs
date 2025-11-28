caf5b1f1efd9576849ab13a4245f387ba13d1529
lemonmel-2 <maychin0205@gmail.com>
2025-11-28 14:23:34 +0800

    system file

--- obj/Debug/net8.0/consoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/consoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("consoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d844d229d1ef2ccd920324155e5d3f775a31b37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+929982d2158e6fa4f6dc324382597582e302300c")]
 [assembly: System.Reflection.AssemblyProductAttribute("consoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("consoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
